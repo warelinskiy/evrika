e736fd5e32758d7579fbf4575c030facdeefbccf
warelinskiy <s.baiturcom@gmail.com>
2026-03-28 00:47:33 +0600

    Updated App.jsx with React Router setup for evrika educational platform

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
